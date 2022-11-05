0e618d8045e9a0c7fc577eeecaaaef80249d9ba7
nicodeheza <nicolasdeheza@hotmail.com>
2022-11-05 16:43:49 -0300

    cheese token

--- contracts/CheeseToken.sol
+++ contracts/CheeseToken.sol
@@ -1,4 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract CheeseToken {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+error CheeseToken__forbidden();
+
+contract CheeseToken is ERC20 {
+    address private immutable i_gameAddress;
+
+    constructor(address gameAddress, uint256 supply)
+        ERC20("CheeseToken", "CT")
+    {
+        i_gameAddress = gameAddress;
+        _mint(gameAddress, supply);
+    }
+
+    function transfer(address to, uint256 amount)
+        public
+        override
+        onlyGame
+        returns (bool)
+    {
+        _transfer(i_gameAddress, to, amount);
+        _approve(to, i_gameAddress, amount);
+        return true;
+    }
+
+    function approve(address, uint256)
+        public
+        override
+        onlyGame
+        returns (bool)
+    {}
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override onlyGame returns (bool) {
+        _spendAllowance(from, i_gameAddress, amount);
+        _approve(to, i_gameAddress, allowance(to, i_gameAddress) + amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue)
+        public
+        override
+        onlyGame
+        returns (bool)
+    {}
+
+    function increaseAllowance(address spender, uint256 addedValue)
+        public
+        override
+        onlyGame
+        returns (bool)
+    {}
+
+    modifier onlyGame() {
+        if (_msgSender() != i_gameAddress) revert CheeseToken__forbidden();
+        _;
+    }
+}

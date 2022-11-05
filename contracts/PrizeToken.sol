d2ef8540fd51a5423645b253c3c8ee2cec0dc758
nicodeheza <nicolasdeheza@hotmail.com>
2022-11-05 16:55:43 -0300

    prize token

--- contracts/PrizeToken.sol
+++ contracts/PrizeToken.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract PrizeToken {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+error PrizeToken__forbidden();
+
+contract PrizeToken is ERC20 {
+    address private immutable i_gameAddress;
+
+    constructor(address gameAddress) ERC20("MouseGamePrice", "MGP") {
+        i_gameAddress = gameAddress;
+    }
+
+    function mint(uint256 amount) public onlyGame {
+        _mint(i_gameAddress, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyGame {
+        _burn(account, amount);
+    }
+
+    modifier onlyGame() {
+        if (_msgSender() != i_gameAddress) revert PrizeToken__forbidden();
+        _;
+    }
+}

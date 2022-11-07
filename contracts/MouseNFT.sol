dce9d1c4463d88f54ad5bf493a00c2398ea12309
nicodeheza <nicolasdeheza@hotmail.com>
2022-11-07 20:40:54 -0300

    mouse nft

--- contracts/MouseNFT.sol
+++ contracts/MouseNFT.sol
@@ -4,11 +4,23 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "./GameMinion.sol";
+import "./CheeseToken.sol";
 
-contract MouseNFT is ERC721 {
-    constructor() ERC721("Mouse", "M") {}
+error MouseNFT__OnlyOneMouse();
 
+contract MouseNFT is ERC721, GameMinion {
     uint256 s_tokenCount = 0;
+    uint256 s_liveTokens = 0;
+    uint256 s_lastTranfer;
+    CheeseToken cheeseToken;
+
+    constructor(address gameAddress, address cheeseTokenAddress)
+        ERC721("Mouse", "M")
+        GameMinion(gameAddress)
+    {
+        cheeseToken = CheeseToken(cheeseTokenAddress);
+    }
 
     function tokenURI(uint256) public pure override returns (string memory) {
         bytes memory dataURI = abi.encodePacked(
@@ -30,5 +42,34 @@ contract MouseNFT is ERC721 {
                     Base64.encode(dataURI)
                 )
             );
+    }
+
+    function mint(address to) external onlyGame {
+        if (s_liveTokens > 0) revert MouseNFT__OnlyOneMouse();
+        _mint(to, s_tokenCount);
+        s_liveTokens++;
+        s_tokenCount++;
+    }
+
+    function burn() external onlyGame {
+        _burn(s_tokenCount);
+        s_liveTokens = 0;
+    }
+
+    function _beforeTokenTransfer(
+        address,
+        address to,
+        uint256
+    ) internal override {
+        if (s_lastTranfer > 0) {
+            uint256 tokensToSteal = (block.timestamp - s_lastTranfer) / 30;
+            address owner = ownerOf(s_tokenCount);
+            cheeseToken.transferFrom(owner, address(this), tokensToSteal);
+        }
+        if (to == address(0)) {
+            s_lastTranfer = 0;
+        } else {
+            s_lastTranfer = block.timestamp;
+        }
     }
 }

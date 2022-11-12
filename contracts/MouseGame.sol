41b2a779b1f0c7217182350623f9d5771da54286
nicodeheza <nicolasdeheza@hotmail.com>
2022-11-12 19:25:23 -0300

    referee

--- contracts/MouseGame.sol
+++ contracts/MouseGame.sol
@@ -15,32 +15,31 @@ error MouseGame__trasactionFail();
 error MouseGame__tryAgainLater();
 error MouseGame__gameInProgress();
 error MouseGame__underfunded();
-
-// review game state
+error MouseGame__OnlyReferee();
 
 contract MouseGame is RandomNumber {
-    enum GameState {
-        open,
-        playing,
-        close
-    }
     struct Winner {
         uint256 balance;
         address player;
     }
 
-    CheeseToken cheeseToken;
-    PrizeToken prizeToken;
-    GameState private s_gameSatate = GameState.open;
     uint256 constant ENTRANCE_FEE = 0.0014 ether;
-    address[] private s_players;
-    uint256 private s_inscriptionStartTime;
-    uint256 private s_gameStartTime;
     uint256 constant INSCRIPTION_LIMIT = 10 minutes;
     uint256 constant GAME_DURATION = 2 hours;
     uint256 constant CHEESE_INITIAL_AMOUNT = 240;
     uint256 constant MAX_PLAYERS = 10;
     uint256 constant MIN_PLAYERS = 3;
+    uint256 constant REFEREE_INITIAL_PERCENTAGE = 5;
+
+    address public immutable i_referee;
+
+    address[] private s_players;
+    uint256 private s_inscriptionStartTime;
+    uint256 private s_gameStartTime;
+    mapping(address => uint) private s_balance;
+
+    CheeseToken cheeseToken;
+    PrizeToken prizeToken;
     MouseNFT mouseNft;
 
     constructor(
@@ -49,11 +48,13 @@ contract MouseGame is RandomNumber {
         address linkAddressm,
         address wrapperAddress,
         address uniswapRouterAddress,
-        address mouseNftAddress
+        address mouseNftAddress,
+        address referee
     ) RandomNumber(linkAddressm, wrapperAddress, uniswapRouterAddress) {
         cheeseToken = CheeseToken(cheeseTokenAddress);
         prizeToken = PrizeToken(prizeTokenAddress);
         mouseNft = MouseNFT(mouseNftAddress);
+        i_referee = referee;
     }
 
     event playerInscribed(uint256 indexed time, address player);
@@ -62,16 +63,13 @@ contract MouseGame is RandomNumber {
     event gameEnded(address indexed player, uint256 playerPrize);
     event gameWinner(address winner);
     event prizeSwaped(address indexed player, uint256 prize, uint256 eth);
+    event refereeWithdrawEvent(uint256 amount);
 
     function inscribe() public payable {
-        if (
-            s_gameSatate == GameState.close ||
-            getInscriptionTimeLeft() == 0 ||
-            s_players.length >= MAX_PLAYERS
-        ) {
-            if (s_gameSatate == GameState.open) s_gameSatate = GameState.close;
+        if (getInscriptionTimeLeft() == 0 || s_players.length >= MAX_PLAYERS) {
             revert MouseGame__inscriptionClose();
         }
+
         if (msg.value < ENTRANCE_FEE) revert MouseGame__underpayment();
         if (msg.value > ENTRANCE_FEE) revert MouseGame__overpaid();
         if (isRegistered(msg.sender)) revert MouseGame__alreadyRegistered();
@@ -86,13 +84,11 @@ contract MouseGame is RandomNumber {
         emit playerInscribed(block.timestamp, msg.sender);
     }
 
-    function startGame() public {
-        if (s_gameSatate != GameState.close || getInscriptionTimeLeft() > 0)
-            revert MouseGame__notReadyToStart();
+    function startGame() public onlyReferee {
+        if (getInscriptionTimeLeft() > 0) revert MouseGame__notReadyToStart();
         if (s_players.length < MIN_PLAYERS) {
             revertGame();
         } else {
-            if (s_gameSatate != GameState.close) s_gameSatate = GameState.close;
             requestRandomWords();
         }
     }
@@ -105,7 +101,6 @@ contract MouseGame is RandomNumber {
         address mouseOwner = s_players[mouseOwnerIndex];
         mouseNft.mint(mouseOwner);
         s_gameStartTime = block.timestamp;
-        s_gameSatate = GameState.playing;
 
         emit gameStarted(block.timestamp, mouseOwner);
     }
@@ -125,11 +120,12 @@ contract MouseGame is RandomNumber {
             }
         }
 
+        s_inscriptionStartTime = 0;
         emit gameReverted();
     }
 
-    function endGame() public {
-        if (getGameTimeLeft() > 0 || s_gameSatate != GameState.playing) {
+    function endGame() public onlyReferee {
+        if (getGameTimeLeft() > 0) {
             revert MouseGame__gameInProgress();
         }
         Winner memory winner = Winner(0, address(0));
@@ -160,7 +156,32 @@ contract MouseGame is RandomNumber {
         prizeToken.mint(winner.player, mouseCheeseBalance);
         mouseNft.burn();
 
-        s_gameSatate = GameState.open;
+        uint256 startDelay = getDelay(
+            s_gameStartTime,
+            s_inscriptionStartTime,
+            INSCRIPTION_LIMIT
+        );
+        uint256 endDelay = getDelay(
+            block.timestamp,
+            s_gameStartTime,
+            GAME_DURATION
+        );
+
+        uint256 totalMinutesDelay = (startDelay + endDelay) / 60;
+        uint256 refereePercentage;
+        if (totalMinutesDelay >= REFEREE_INITIAL_PERCENTAGE) {
+            refereePercentage = 0;
+        } else {
+            refereePercentage = refereePercentage - totalMinutesDelay;
+        }
+
+        uint256 refereeBalance = (refereePercentage * address(this).balance) /
+            100;
+        uint256 gameBalance = address(this).balance - refereeBalance;
+
+        s_balance[i_referee] = refereeBalance;
+        s_balance[address(this)] = gameBalance;
+
         s_gameStartTime = 0;
         s_inscriptionStartTime = 0;
         delete s_players;
@@ -168,18 +189,30 @@ contract MouseGame is RandomNumber {
         emit gameWinner(winner.player);
     }
 
-    // add event
     function prizeToEth(uint256 amount) public payable {
         uint256 userBalance = prizeToken.balanceOf(msg.sender);
         if (amount > userBalance) revert MouseGame__underfunded();
         prizeToken.burn(msg.sender, amount);
         uint256 prizeTokenValue = getPrizeTokenValue();
         uint256 ethToSend = amount * prizeTokenValue;
+        s_balance[address(this)] = s_balance[address(this)] - ethToSend;
+
         (bool sent, ) = msg.sender.call{value: ethToSend}("");
 
         if (!sent) revert MouseGame__trasactionFail();
 
         emit prizeSwaped(msg.sender, amount, ethToSend);
+    }
+
+    function refereeWithdraw() public payable onlyReferee {
+        uint256 amount = s_balance[i_referee];
+        s_balance[i_referee] = 0;
+
+        (bool sent, ) = msg.sender.call{value: amount}("");
+
+        if (!sent) revert MouseGame__trasactionFail();
+
+        emit refereeWithdrawEvent(amount);
     }
 
     function isRegistered(address player) internal view returns (bool) {
@@ -191,13 +224,9 @@ contract MouseGame is RandomNumber {
 
     function getPrizeTokenValue() public view returns (uint256 ethAmount) {
         uint256 prizeTokenTotalSupply = prizeToken.totalSupply();
-        uint256 thisBalance = address(this).balance;
+        uint256 thisBalance = s_balance[address(this)];
 
         return thisBalance / prizeTokenTotalSupply;
-    }
-
-    function getGameState() public view returns (GameState) {
-        return s_gameSatate;
     }
 
     function getInscriptionTimeLeft() public view returns (uint256) {
@@ -212,5 +241,28 @@ contract MouseGame is RandomNumber {
         uint256 time = block.timestamp - s_gameStartTime;
         if (time >= GAME_DURATION) return 0;
         return GAME_DURATION - time;
+    }
+
+    function getGameBalance() public view returns (uint256) {
+        return s_balance[address(this)];
+    }
+
+    function getRefereeBalance() public view onlyReferee returns (uint256) {
+        return s_balance[i_referee];
+    }
+
+    function getDelay(
+        uint256 endTime,
+        uint256 startTime,
+        uint256 duration
+    ) internal pure returns (uint256 delay) {
+        int delayTime = int(endTime) - int(startTime) - int(duration);
+        if (delayTime < 0) delayTime = delayTime * -1;
+        return uint256(delayTime);
+    }
+
+    modifier onlyReferee() {
+        if (msg.sender != i_referee) revert MouseGame__OnlyReferee();
+        _;
     }
 }

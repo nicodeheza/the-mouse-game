a4dbcffb44128d5c594f3527601f78bc7ba75320
nicodeheza <nicolasdeheza@hotmail.com>
2022-11-15 15:27:15 -0300

    fix deploy

--- deploy/05-set-contracts.ts
+++ deploy/05-set-contracts.ts
@@ -1,6 +1,6 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
-import {getContractAddress, setContractAddress} from "../scripts/contractsAddress";
+import {getContractAddress} from "../scripts/contractsAddress";
 import {ethers} from "hardhat";
 
 const setContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -11,15 +11,32 @@ const setContracts: DeployFunction = async function (hre: HardhatRuntimeEnvironm
 
 	const contractsAddress = getContractAddress()[networkName];
 
-	const game = await ethers.getContract("MouseGame", deployer);
-	await game.setContracts(
-		contractsAddress.MouseNFT[0],
-		contractsAddress.CheeseToken[0],
-		contractsAddress.PrizeToken[0]
-	);
+	try {
+		const game = await ethers.getContract("MouseGame", deployer);
+		const gameTx = await game.setContracts(
+			contractsAddress.MouseNFT[0],
+			contractsAddress.CheeseToken[0],
+			contractsAddress.PrizeToken[0]
+		);
 
-	const mouse = await ethers.getContract("MouseNFT", deployer);
-	await mouse.setCheeseToken(contractsAddress.CheeseToken[0]);
+		const gameReceipt = await gameTx.wait();
+		console.log(
+			"Game contracts seated with",
+			ethers.utils.formatEther(gameReceipt.gasUsed.mul(gameReceipt.effectiveGasPrice)),
+			"eth gas."
+		);
+		const mouse = await ethers.getContract("MouseNFT", deployer);
+		const mouseTx = await mouse.setCheeseToken(contractsAddress.CheeseToken[0]);
+		const mouseReceipt = await mouseTx.wait();
+
+		console.log(
+			"Mouse cheeseToken seated with",
+			ethers.utils.formatEther(mouseReceipt.gasUsed.mul(mouseReceipt.effectiveGasPrice)),
+			"eth gas."
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export default setContracts;
--- scripts/contractsAddress.ts
+++ scripts/contractsAddress.ts
@@ -1,6 +1,6 @@
 import {readFileSync, writeFileSync} from "fs";
 
-const JSON_PATH = "../data/contractsAddress.json";
+const JSON_PATH = "./data/contractsAddress.json";
 
 export function getContractAddress() {
 	return JSON.parse(readFileSync(JSON_PATH, "utf8"));
@@ -8,6 +8,7 @@ export function getContractAddress() {
 
 export function setContractAddress(chain: string, name: string, address: string) {
 	const contractsAddress = getContractAddress();
+	if (contractsAddress[chain][name][0] === address) return;
 	contractsAddress[chain][name] = [
 		address,
 		...(contractsAddress[chain][name] ? contractsAddress[chain][name] : [])

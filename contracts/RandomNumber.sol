40be0e0209bbb7bf1bd5eb4da7402d14c94ffb96
nicodeheza <nicolasdeheza@hotmail.com>
2022-11-23 19:24:10 -0300

    linkToGet 0

--- contracts/RandomNumber.sol
+++ contracts/RandomNumber.sol
@@ -20,7 +20,12 @@ abstract contract RandomNumber is VRFV2WrapperConsumerBase, SwapEthToLink {
 
     function requestRandomWords() internal returns (uint256 requestID) {
         uint256 price = VRF_V2_WRAPPER.calculateRequestPrice(callbackGasLimit);
-        uint256 linkToGet = price - s_linkBalance;
+        uint256 linkToGet;
+        if (price - s_linkBalance < 0) {
+            linkToGet = 0;
+        } else {
+            linkToGet = price - s_linkBalance;
+        }
         convertEthToLink(linkToGet);
         if (s_linkBalance < price) revert RandomNumber__insufficientFunds();
         return requestRandomness(callbackGasLimit, 3, 1);

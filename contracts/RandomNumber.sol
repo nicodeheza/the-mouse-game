1b6db6b40c414034f5e457c3c5f299c3e635bcb7
nicodeheza <nicolasdeheza@hotmail.com>
2022-12-08 15:13:10 -0300

    block confirmations

--- contracts/RandomNumber.sol
+++ contracts/RandomNumber.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.9;
 import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
 import "./SawpEthToLink.sol";
 
+import "hardhat/console.sol";
+
 error RandomNumber__insufficientFunds();
 
 abstract contract RandomNumber is VRFV2WrapperConsumerBase, SwapEthToLink {
-    uint32 callbackGasLimit = 100000;
+    uint32 constant callbackGasLimit = 100000;
 
     constructor(
         address linkAdderess,
@@ -28,6 +30,6 @@ abstract contract RandomNumber is VRFV2WrapperConsumerBase, SwapEthToLink {
         }
         convertEthToLink(linkToGet);
         if (s_linkBalance < price) revert RandomNumber__insufficientFunds();
-        return requestRandomness(callbackGasLimit, 3, 1);
+        return requestRandomness(callbackGasLimit, 1, 1);
     }
 }
